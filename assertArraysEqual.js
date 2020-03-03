b2d321927876e3c1bdb17c370398824d31c59b7f
mcagan <meliscagan@gmail.com>
2020-03-03 23:25:39 +0000

    fixed stylistic errors

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,15 @@
 const eqArrays = function(array1, array2) {
-  if(array1.length !== array2.length){
+  if (array1.length !== array2.length) {
     return false;
   } else {
-    for(i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]){
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
         return false;
-      } 
+      }
     }
     return true;
   }
-}
+};
 
 const assertArraysEqual = function(arrayA, arrayB) {
   if (eqArrays(arrayA, arrayB) === true) {
