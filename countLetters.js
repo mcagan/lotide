a183b31dfabc1d1a6496f578ab96875869110c06
mcagan <meliscagan@gmail.com>
2020-03-04 18:38:54 +0000

    function to count the letters in a sentence

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let newStc = sentence.replace(/\s+/g, '');
+  let results = {};
+  for (let letter of newStc) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
+
+console.log(countLetters("lighthouse in the house"));
+console.log(countLetters("melis melis melis"));
+console.log(countLetters("portland maine"));
