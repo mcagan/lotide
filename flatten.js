9eaec407a73d827ed706b69ad9874537864bdb58
mcagan <meliscagan@gmail.com>
2020-03-04 04:17:16 +0000

    function to flatten nested arrays

---
+++ flatten.js
@@ -0,0 +1,37 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(arrayA, arrayB) {
+  if (eqArrays(arrayA, arrayB) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arrayA} === ${arrayB}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arrayA} !== ${arrayB}`);
+  }
+};
+
+const flatten = function(array) {
+  let newArray = [];
+  for (let item of array) {
+    if (Array.isArray(item) === true) {
+      newArray = newArray.concat(item);
+    } else {
+      newArray.push(item);
+    }
+  }
+  return newArray;
+};
+
+assertArraysEqual(flatten([2, 3, [5, 6, 9], 8]), [2, 3, 5, 6, 9, 8]);
+assertArraysEqual(flatten([2, 3, 8]), [2, 3, 8]);
+assertArraysEqual(flatten([2, 3, "boop", [3], ["Irmak"]]), [2, 3, "boop", 3, "Irmak"]);
+assertArraysEqual(flatten([]), []);
