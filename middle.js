96e664ce9fd15dbea8467a07a23660c3d1be5041
mcagan <meliscagan@gmail.com>
2020-03-04 03:33:37 +0000

    fixed stylistic issues

--- middle.js
+++ middle.js
@@ -1,15 +1,15 @@
 const eqArrays = function(array1, array2) {
-  if(array1.length !== array2.length){
+  if (array1.length !== array2.length) {
     return false;
   } else {
-    for(i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]){
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
         return false;
-      } 
+      }
     }
     return true;
   }
-}
+};
 
 const assertArraysEqual = function(arrayA, arrayB) {
   if (eqArrays(arrayA, arrayB) === true) {
@@ -20,19 +20,19 @@ const assertArraysEqual = function(arrayA, arrayB) {
 };
 
 const middle = function(array) {
-  let result = []
+  let result = [];
   if (array.length <= 2) {
     return result;
   } else if (array.length % 2 !== 0) {
-    index = (array.length - 1) / 2;
+    let index = (array.length - 1) / 2;
     result.push(array[index]);
   } else {
-    index = array.length / 2;
+    let index = array.length / 2;
     result.push(array[index - 1]);
     result.push(array[index]);
   }
   return result;
-}
+};
 
 assertArraysEqual(middle([1, 2, 3]), [2]);
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
