ed24532a0bf318c5438d449512f4bce66cae20f7
mcagan <meliscagan@gmail.com>
2020-03-09 17:34:49 +0000

    added test file and implemented module exports

--- middle.js
+++ middle.js
@@ -1,23 +1,6 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = function(arrayA, arrayB) {
-  if (eqArrays(arrayA, arrayB) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${arrayA} === ${arrayB}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arrayA} !== ${arrayB}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(array) {
   let result = [];
@@ -34,7 +17,4 @@ const middle = function(array) {
   return result;
 };
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+module.exports = middle;
